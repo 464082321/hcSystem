7f119d88fba8b15397f6052b0f1455900f7e33a3
zhang.hc <zhang.hc@yougou.com>
2016-02-02 17:30:22 +0800

    Spring Mvc 初步搭建

---
+++ src/test/java/com/john/test/JUnitTest.java
@@ -0,0 +1,15 @@
+package com.john.test;
+
+import org.junit.Test;
+
+/**
+ * 单元测试环境
+ * @author zhang.hc
+ *
+ */
+public class JUnitTest {
+	@Test
+	public void test() {
+		System.out.println("我的第一个单元测试.");
+	}
+}
